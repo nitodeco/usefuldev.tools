a7a085121142e2f012a185b34a4aab03c5932361
nitodeco <n.moehn@outlook.com>
2025-07-29 02:26:34 +0200

    fix: add missing posthog configs

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,25 @@ const withNextIntl = createNextIntlPlugin({
   },
 });
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/flags',
+        destination: 'https://eu.i.posthog.com/flags',
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+};
 
 export default withNextIntl(nextConfig);
--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,6 +1,9 @@
 import posthog from 'posthog-js';
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+  api_host: '/ingest',
+  ui_host: 'https://eu.posthog.com',
   defaults: '2025-05-24',
+  capture_exceptions: true,
+  debug: process.env.NODE_ENV === 'development',
 });
